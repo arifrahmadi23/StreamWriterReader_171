a517651f3dc9546ef99c2c800c57549f80e290f0
arifrahmadi23 <muhammad231arif@gmail.com>
2023-06-19 15:40:40 +0700

    awal program dan program try

--- exeptionStandart/exeptionStandart.cpp
+++ exeptionStandart/exeptionStandart.cpp
@@ -1,20 +1,17 @@
-// exeptionStandart.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <exeption>
+//untuk obyek exception yang akan digunakan
+#include <array>
+//untuk obvyek array yang akan kita gunakkan
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cout << "Awal Program" << endl; //penanda 1:...
+	try {
+		array<int, 3> data = { 1,2,3 };
+		//pesan array integer 3 elemen 
+		cout << data.at(4) << endl;
+		//memenggil array elemen ke 5
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
