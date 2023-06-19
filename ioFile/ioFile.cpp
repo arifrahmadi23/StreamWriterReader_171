73d8ffacf66034dda511682ff3eb042580047868
arifrahmadi23 <muhammad231arif@gmail.com>
2023-06-19 14:53:33 +0700

    //menulis dan memasukan nilai dari 'baris' ke dalam file   outfile << baris << endl;  }

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -21,7 +21,7 @@ int main() {
 		//loop akan berhenti jika anda memasukan karakter 
 		if (baris == "q") break;
 		//menulis dan memasukan nilai dari 'baris' ke dalam file
-		outfule << baris << endl;
+		outfile << baris << endl;
 	}
 
 }
